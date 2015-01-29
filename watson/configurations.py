c72d5e9b84b1c25bdbb783e3791542eac24be909
mome <npsjua@gmail.com>
2015-01-29 18:13:00 +0100

    paths fitted to new module folder

--- watson/configurations.py
+++ watson/configurations.py
@@ -2,19 +2,21 @@ from os.path import abspath, dirname, sep
 
 watson_path = dirname(abspath(__file__))
 
+up = sep + '..' + sep
+
 # STANFORD PARSER
-stanford_path = abspath(watson_path + sep + 'stanford-parser-full')
+stanford_path = abspath(watson_path + up + 'stanford-parser-full')
 stanford_parser = stanford_path + sep + 'stanford-parser.jar'
 stanford_models = stanford_path + sep + 'stanford-parser-3.4.1-models.jar'
 
 # STANFORD NAMED ENTITY TAGGER
-stanford_ner_path = abspath(watson_path + sep + 'stanford-ner')
+stanford_ner_path = abspath(watson_path + up + 'stanford-ner')
 #stanford_ner_classifier = stanford_ner_path + sep + 'classifiers' + sep + 'english.all.3class.distsim.crf.ser.gz'
 stanford_ner_classifier = stanford_ner_path + sep + 'classifiers' + sep + 'english.muc.7class.distsim.crf.ser.gz'
 stanford_ner = stanford_ner_path + sep + 'stanford-ner.jar'
 
 # STANFORD PART OF SPEECH TAGGER
-stanford_pos_path = abspath(watson_path + sep + 'stanford-postagger')
+stanford_pos_path = abspath(watson_path + up + 'stanford-postagger')
 stanford_pos_model = stanford_pos_path + sep + 'models' + sep + 'english-bidirectional-distsim.tagger'
 stanford_postagger = stanford_pos_path + sep +'stanford-postagger.jar'
 
